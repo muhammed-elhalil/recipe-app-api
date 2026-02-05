9977eb8d07151ad790831f021c342033085f3948
muhammed-elhalil <muhammedelhalil0@gmail.com>
2026-02-05 14:36:02 +0300

    fix code v1

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,10 @@
-"""
-Calculator functions
-"""
+"""Calculator functions"""
 
 
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x,y):
     """Subtract x from y and return result"""
